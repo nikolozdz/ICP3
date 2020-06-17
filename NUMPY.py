0abb1827a832a6c6b4a7639736a01de937a8d4f0
nikolozdz <48862753+nikolozdz@users.noreply.github.com>
2020-06-16 23:03:19 -0500

    updated numpy

--- NUMPY.py
+++ NUMPY.py
@@ -1,26 +1,14 @@
 import numpy as np
-def exists(x,lst):
-
-    for y in lst:
-        if abs(y-x)<0.0001:
-            return True
-    return False
 
 randomArray=np.random.rand(20)*20
-#print(randomArray)
-
-randomArray=randomArray.reshape(4,5)
 print("Initial")
 print(randomArray)
-
-
-
-#print(randomArray)
-maxValues=np.max(randomArray,axis=1)
 print('\n')
-print("Max Values")
-print(maxValues)
+randomArray=randomArray.reshape(4,5)
+print("After reshape")
+print(randomArray)
 print('\n')
+print('after replacing max values')
 print(np.where(randomArray == np.max(randomArray, axis=1).reshape(-1,1), 0, randomArray))
 
 
